5939e3af7e5a56ef0d749f4d8cf343f7e3791748
Tanh <Anh1412sun@gmail.com>
2025-10-08 22:37:55 +0700

    Remove outdated About, Contact, Privacy, and Terms pages; update package.json and server.js for module support and Handlebars integration.

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 import express from 'express';
-import { dirname, join } from 'path'; // Sửa ở đây: Nhập cả dirname và join theo tên
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+import hbs from 'hbs';
 
 const app = express();
 const port = 3000;
 
-// Lấy đường dẫn thư mục hiện tại một cách chính xác trong ES Modules
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename); // Sửa ở đây: Sử dụng trực tiếp hàm dirname
+const __dirname = dirname(__filename);
 
 app.use(express.static(join(__dirname, 'public')));
+
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+app.set('views', join(__dirname, 'views'));
+hbs.registerPartials(join(__dirname, 'views', 'partials'));
+
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
