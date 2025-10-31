1e842657dc2374fa908b3836df2e7de3e72e66d3
TruongLinhIT <linhtvt.24it@vku.udn.vn>
2025-10-31 22:55:40 +0700

    deleted:    hashPassword.js

--- hashPassword.js
+++
@@ -1,5 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const plainPassword = '123'; // ← mật khẩu thật bạn đang dùng
-const hashed = await bcrypt.hash(plainPassword, 10);
-console.log('Bcrypt hash:', hashed);
